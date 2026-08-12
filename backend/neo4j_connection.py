3d53f614a276882237f3b455d8057645ea8be44a
prasanna <prasanna1@gmail.com>
2026-08-12 16:53:09 +0530

    Update Neo4j connection output

--- backend/neo4j_connection.py
+++ backend/neo4j_connection.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
 
@@ -8,7 +9,6 @@ URI = os.getenv("NEO4J_URI")
 USERNAME = os.getenv("NEO4J_USERNAME")
 PASSWORD = os.getenv("NEO4J_PASSWORD")
 DATABASE = os.getenv("NEO4J_DATABASE", "neo4j")
-
 
 driver = GraphDatabase.driver(
     URI,
@@ -22,6 +22,12 @@ def test_connection():
             "RETURN 'ChronoGraph connection successful!' AS message"
         )
         print(result.single()["message"])
+
+        browser_url = "http://localhost:7474/browser/"
+        print(f"Neo4j Browser: {browser_url}")
+
+        # # Automatically open Neo4j Browser
+        # webbrowser.open(browser_url)
 
 
 if __name__ == "__main__":
